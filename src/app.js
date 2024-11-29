3885b7f7d7e986f9ddceb3115e3e10b8eaac4141
BowserZ <juanpinto07@gmail.com>
2024-11-29 17:52:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Concatenar las partes en una excusa
+  let excuse =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+
+  // Establecer el contenido de #excuse con la nueva excusa
+  document.getElementById("excuse").innerHTML = excuse;
+}
+
+// Ejecutar la función cuando la página se carga
+window.onload = generateExcuse;
